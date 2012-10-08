61f08f319321a49e36b7f05fbf645ec7a1c4eec1
Hiroaki Nakamura <hnakamur@gmail.com>
2012-10-08 11:30:23 +0900

    Set metatable to bitvector.

--- src/bitvector.c
+++ src/bitvector.c
@@ -1,6 +1,10 @@
 #include <lauxlib.h>
 #include <limits.h>
 
+#define BITVECTOR_MTBL_NAME "cs.BitVector"
+#define cs_checkbitvector(L, index) \
+  ((cs_bitvector_t *)luaL_checkudata(L, index, BITVECTOR_MTBL_NAME))
+
 #define BITS_PER_WORD (CHAR_BIT * sizeof(unsigned int))
 #define I_WORD(i) ((unsigned int)(i) / BITS_PER_WORD)
 #define I_BIT(i) (1 << ((unsigned int)(i) % BITS_PER_WORD))
@@ -19,6 +23,8 @@ static int bitvector_new(lua_State *L) {
   luaL_argcheck(L, n >= 1, 1, "invalid length");
   nbytes = sizeof(cs_bitvector_t) + I_WORD(n - 1) * sizeof(unsigned int);
   v = (cs_bitvector_t *)lua_newuserdata(L, nbytes);
+  luaL_getmetatable(L, BITVECTOR_MTBL_NAME);
+  lua_setmetatable(L, -2);
 
   v->length = n;
   for (i = 0; i < I_WORD(n - 1); i++)
@@ -27,33 +33,34 @@ static int bitvector_new(lua_State *L) {
   return 1;
 }
 
-static int bitvector_set(lua_State *L) {
-  cs_bitvector_t *v = (cs_bitvector_t *)lua_touserdata(L, 1);
+static unsigned int *get_entry_and_mask(lua_State *L, unsigned int *mask) {
+  cs_bitvector_t *v = cs_checkbitvector(L, 1);
   int index = luaL_checkint(L, 2) - 1;
-  luaL_checkany(L, 3);
-
-  luaL_argcheck(L, v != NULL, 1, "'bitvector' expected");
   luaL_argcheck(L, 0 <= index && index < v->length, 2, "index out of range");
+  *mask = I_BIT(index);
+  return &v->values[I_WORD(index)];
+}
 
+static int bitvector_set(lua_State *L) {
+  unsigned int mask;
+  unsigned int *entry = get_entry_and_mask(L, &mask);
+  luaL_checkany(L, 3);
   if (lua_toboolean(L, 3))
-    v->values[I_WORD(index)] |= I_BIT(index);
+    *entry |= mask;
   else
-    v->values[I_WORD(index)] &= ~I_BIT(index);
+    *entry &= ~mask;
   return 0;
 }
 
 static int bitvector_get(lua_State *L) {
-  cs_bitvector_t *v = (cs_bitvector_t *)lua_touserdata(L, 1);
-  int index = luaL_checkint(L, 2) - 1;
-  luaL_argcheck(L, v != NULL, 1, "'bitvector' expected");
-
-  luaL_argcheck(L, 0 <= index && index < v->length, 2, "index out of range");
-  lua_pushboolean(L, v->values[I_WORD(index)] & I_BIT(index));
+  unsigned int mask;
+  unsigned int *entry = get_entry_and_mask(L, &mask);
+  lua_pushboolean(L, *entry & mask);
   return 1;
 }
 
 static int bitvector_length(lua_State *L) {
-  cs_bitvector_t *v = (cs_bitvector_t *)lua_touserdata(L, 1);
+  cs_bitvector_t *v = cs_checkbitvector(L, 1);
   luaL_argcheck(L, v != NULL, 1, "'bitvector' expected");
   lua_pushinteger(L, v->length);
   return 1;
@@ -68,6 +75,7 @@ static const struct luaL_Reg bitvector_functions[] = {
 };
 
 int luaopen_cs_bitvector(lua_State *L) {
+  luaL_newmetatable(L, BITVECTOR_MTBL_NAME);
   luaL_register(L, "bitvector", bitvector_functions);
   lua_pop(L, 1);
   return 1;
--- test/bitvector.lua
+++ test/bitvector.lua
@@ -2,13 +2,21 @@ local exports = {}
 
 exports['bitvector'] = function(test)
   local cs = require('cornerstone')
-  local a = bitvector.new(1000)
-  test.equal(type(a), 'userdata')
-  test.equal(bitvector.length(a), 1000)
+  local v = bitvector.new(1000)
+  test.equal(type(v), 'userdata')
+  test.equal(bitvector.length(v), 1000)
   for i = 1, 1000 do
-    bitvector.set(a, i, i % 5 == 0)
+    bitvector.set(v, i, i % 5 == 0)
   end
-  test.ok(bitvector.get(a, 10))
+  test.ok(bitvector.get(v, 10))
+  test.done()
+end
+
+exports['bitvector.checkudata'] = function(test)
+  local cs = require('cornerstone')
+  local ok, err = pcall(bitvector.get, io.stdin, 10)
+  test.ok(not ok)
+  test.is_string(err)
   test.done()
 end
 
